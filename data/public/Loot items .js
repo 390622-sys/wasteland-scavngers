7ef8c1e0136649a39d98c9ec2ef64a45b54d426d
390622-sys <390622@guhsd.net>
2026-05-28 17:30:03 +0000

    #04-Extended-Scavenger-Profile

---
+++ data/public/Loot items .js
@@ -0,0 +1,86 @@
+[
+  {
+    "id": "loot-001",
+    "userId": "user-001",
+    "name": "Scrap Metal",
+    "type": "material",
+    "rarity": "common",
+    "value": 5,
+    "weight": 2,
+    "description": "Twisted chunks of pre-war steel. Useful for repairs.",
+    "foundInZone": "zone-003",
+    "foundAt": "2077-11-15T10:22:00.000Z"
+  },
+  {
+    "id": "loot-002",
+    "userId": "user-001",
+    "name": "Canned Brahmin Stew",
+    "type": "food",
+    "rarity": "common",
+    "value": 12,
+    "weight": 1,
+    "description": "Two-headed cow stew. Still good... probably.",
+    "foundInZone": "zone-002",
+    "foundAt": "2077-11-16T14:05:00.000Z"
+  },
+  {
+    "id": "loot-003",
+    "userId": "user-002",
+    "name": "Laser Pistol",
+    "type": "weapon",
+    "rarity": "uncommon",
+    "value": 150,
+    "weight": 3,
+    "description": "Pre-war energy weapon. Needs a charge pack.",
+    "foundInZone": "zone-001",
+    "foundAt": "2077-11-10T09:00:00.000Z"
+  },
+  {
+    "id": "loot-004",
+    "userId": "user-002",
+    "name": "Rad-Away",
+    "type": "medicine",
+    "rarity": "uncommon",
+    "value": 80,
+    "weight": 0,
+    "description": "Flushes radiation from the body. Tastes awful.",
+    "foundInZone": "zone-002",
+    "foundAt": "2077-11-11T13:40:00.000Z"
+  },
+  {
+    "id": "loot-005",
+    "userId": "user-003",
+    "name": "Mutant Fungus",
+    "type": "food",
+    "rarity": "rare",
+    "value": 35,
+    "weight": 0,
+    "description": "Glows faintly. Either edible or dangerous. Maybe both.",
+    "foundInZone": "zone-001",
+    "foundAt": "2077-11-20T07:15:00.000Z"
+  },
+  {
+    "id": "loot-006",
+    "userId": "user-004",
+    "name": "Water Purifier Filter",
+    "type": "material",
+    "rarity": "uncommon",
+    "value": 60,
+    "weight": 1,
+    "description": "Salvaged filter from an old purifier unit.",
+    "foundInZone": "zone-003",
+    "foundAt": "2077-11-18T11:00:00.000Z"
+  },
+  {
+    "id": "loot-007",
+    "userId": "user-001",
+    "name": "Power Armor Helmet",
+    "type": "armor",
+    "rarity": "legendary",
+    "value": 500,
+    "weight": 10,
+    "description": "T-51 helmet. Cracked visor but still protects.",
+    "foundInZone": "zone-001",
+    "foundAt": "2077-11-22T16:30:00.000Z"
+  }
+]
